e8dc37cf203f0bc5175548e9d3ff117802b2ba33
jll9qw <46170601+jll9qw@users.noreply.github.com>
2019-06-16 03:04:28 -0400

    Add files via upload

---
+++ assets/javascript/flight.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+var api = C7hBoDH9a2PeKiIzWkKrfRAYUlQovG26;
+
+var queryUrl=  "https://test.api.amadeus.com/v1/shopping/flight-dates?origin=MAD&destination=MUC&api_key=C7hBoDH9a2PeKiIzWkKrfRAYUlQovG26&max=5";
+ 
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+      }).then();
+      
+
+
+});
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
